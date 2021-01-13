729d1a27b7cf1c5ae387b919bf3ab9e16688d6b0
Katrina Pritchard <pritckat@gmail.com>
2021-01-13 17:19:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function mondayWork(activity = 'go to the office') {
   return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(str) {
+function wrapAdjective(str='*') {
   return function(x="special") {
     return `You are ${str}${x}${str}!`
   }
